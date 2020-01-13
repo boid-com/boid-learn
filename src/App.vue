1d9ec5f916fe6eda3906ca335a3e042aa714919e
I_Seth <mchosc@animusystems.com>
2020-01-13 07:46:00 +0000

    install

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header reveal elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
+          </q-avatar>
+          Title
+        </q-toolbar-title>
+
+        <q-btn dense flat round icon="menu" @click="right = !right" />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="right" side="right" overlay elevated>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      right: false
+    }
+  }
+}
+</script>
